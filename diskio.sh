6ca5401dd7b9e4bd11726fb74342897a06236b02
Dzivak <levanchuk@gmail.com>
2025-09-25 11:06:40 +0300

    add compatibility with old version of sar

--- diskio.sh
+++ diskio.sh
@@ -30,42 +30,50 @@ detect_disk_type() {
 
 type=$(detect_disk_type "$disk")
 
-LC_ALL=C sar -dp 1 1 | grep "Average:" | awk -v d="$disk" -v t="$type" '
+sector_size=$(blockdev --getss "/dev/$disk" 2>/dev/null || echo 512)
+
+LC_ALL=C sar -dp 1 1 | grep "Average:" | awk -v d="$disk" -v t="$type" -v ss="$sector_size" '
 BEGIN {
   found = 0
   headerParsed = 0
-  # Ініцыялізуем усе пазіцыі як 0
-  tps = rkb = wkb = arq = aqu = awt = svctm = util = dev = 0
+  tps = rkb = wkb = rdsec = wrsec = arq = aqu = awt = svctm = util = dev = 0
 }
 
-# Загалоўкі: вызначаем пазіцыі слупкоў
+# Вызначаем пазіцыі слупкоў
 /^Average:/ && /DEV/ {
   for (i = 1; i <= NF; i++) {
-    if ($i == "tps")      tps = i
-    if ($i == "rkB/s")    rkb = i
-    if ($i == "wkB/s")    wkb = i
-    if ($i == "areq-sz")  arq = i
-    if ($i == "aqu-sz")   aqu = i
-    if ($i == "await")    awt = i
-    if ($i == "svctm")    svctm = i
-    if ($i == "%util")    util = i
-    if ($i == "DEV")      dev = i
+    if ($i == "tps")        tps = i
+    if ($i == "rkB/s")      rkb = i
+    if ($i == "wkB/s")      wkb = i
+    if ($i == "rd_sec/s")   rdsec = i
+    if ($i == "wr_sec/s")   wrsec = i
+    if ($i == "areq-sz")    arq = i
+    if ($i == "aqu-sz")     aqu = i
+    if ($i == "await")      awt = i
+    if ($i == "svctm")      svctm = i
+    if ($i == "%util")      util = i
+    if ($i == "DEV")        dev = i
   }
   headerParsed = 1
   next
 }
 
-# Даныя: шукаем радок, дзе любы слупок роўны d
+# Апрацоўка радка з данымі
 headerParsed && /^Average:/ {
   for (i = 1; i <= NF; i++) {
     if ($i == d) {
       found = 1
+
+      # Вызначаем значэнні чытання/запісу ў кілабайтах
+      read_kb = (rkb ? $(rkb) : (rdsec ? $(rdsec) * ss / 1024 : 0))
+      write_kb = (wkb ? $(wkb) : (wrsec ? $(wrsec) * ss / 1024 : 0))
+
       print "{"
       print "\"disk\":\"" d "\","
       print "\"type\":\"" t "\","
       print "\"tps\":" (tps     ? $(tps)   : 0) ","
-      print "\"rd_sec_per_s\":" (rkb     ? $(rkb)   : 0) ","
-      print "\"wr_sec_per_s\":" (wkb     ? $(wkb)   : 0) ","
+      print "\"read_kB_per_s\":"  read_kb ","
+      print "\"write_kB_per_s\":" write_kb ","
       print "\"avgrq_sz\":"     (arq     ? $(arq)   : 0) ","
       print "\"avgqu_sz\":"     (aqu     ? $(aqu)   : 0) ","
       print "\"await\":"        (awt     ? $(awt)   : 0) ","
